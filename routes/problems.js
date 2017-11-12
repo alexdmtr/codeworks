7bee1b025aaff1d54e7bc0631696f11f173e57c4
Alex Dumitru <alexadrian.dmtr@gmail.com>
2017-11-12 03:25:59 +0000

    Add sandbox var

--- routes/problems.js
+++ routes/problems.js
@@ -13,11 +13,18 @@ exports.getProblems = async (req, res) => {
 }
 
 exports.getProblem = async (req, res) => {
-  var problemID = req.params.id
+  var data = await db.utils.getProblemCode({
+    userID: req.user.id,
+    problem: req.params.id,
+  })
 
-  var problem = await db.utils.getObj("/problems/"+problemID)
-
-  res.render('problems/problem', { problem })
+  data = data || { code: null, args: ""}
+  res.render('sandbox', {
+    jwt: req.cookies['access_token'],
+    code: data.code || "public class Main {\n  public static void main(String[] args) {\n    System.out.println(\"Hello World!\");\n  }\n}",
+    args: data.args,
+    sandbox: false
+  })
 }
 
 exports.postProblem = async (req, res) => {
@@ -45,9 +52,13 @@ exports.getSandbox = async (req, res) => {
     problem: 'sandbox',
   })
 
+  data = data || { code: null, args: ""}
+  
+
   res.render('sandbox', {
     jwt: req.cookies['access_token'],
     code: data.code || "public class Main {\n  public static void main(String[] args) {\n    System.out.println(\"Hello World!\");\n  }\n}",
-    args: data.args
+    args: data.args,
+    sandbox: true
   })
 }
